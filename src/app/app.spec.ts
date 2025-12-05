3741c2e442f02b3e6898e939a32746ab09fb4f24
Bharath_tn <steja11100@gmail.com>
2025-12-05 00:00:11 -0500

    testing assignment

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -7,9 +7,33 @@ describe('app file',()=>{
     expect(app.add(2,3)).toBe(5);
   })
 
-  it('sumOfDigits',()=>{
-    
+  //1.
+  it('sumOfDigits for positive no',()=>{
+    expect(app.sumOfDigits(123)).toBe(6);
+  });
 
-  })
 
+  //2
+  it('factorial',()=>{
+    expect(app.factorial(5)).toBe(120);
+  });
+
+  //3
+  it('isPrime',()=>{
+    expect(app.isPrime(7)).toBeTrue();
+  });
+
+  it('isNotPrime',()=>{
+    expect(app.isPrime(10)).toBeFalse();
+  });
+
+  //5
+  it('gcd',()=>{
+    expect(app.gcd(48,18)).toBe(6);
+  });
+
+  //6
+  it('reverse no',()=>{
+    expect(app.reverseNumber(12345)).toBe(54321);
+  });
 })
