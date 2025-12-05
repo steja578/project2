be893a0167320b14c0786f1278e72e6b7b09481d
Bharath_tn <steja11100@gmail.com>
2025-12-04 23:09:10 -0500

    test

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,8 +1,15 @@
 import { App } from "./app";
 
-describe('apec file',()=>{
-  it('its hsould add',()=>{
-    const app=new App();
+describe('app file',()=>{
+  const app=new App();
+  it('its Should add',()=>{
+    
     expect(app.add(2,3)).toBe(5);
   })
+
+  it('sumOfDigits',()=>{
+    
+
+  })
+
 })
--- src/app/app.ts
+++ src/app/app.ts
@@ -13,4 +13,59 @@ export class App {
   add(a:number,b:number){
     return a+b;
   }
+
+    // 1. Sum of Digits
+  sumOfDigits(num: number) {
+    return Math.abs(num)
+      .toString()
+      .split('')
+      .reduce((sum, digit) => sum + Number(digit), 0);
+  }
+
+  // 2. Factorial
+  factorial(n: number) {
+    if (n < 0) return null;
+    let result = 1;
+    for (let i = 2; i <= n; i++) result *= i;
+    return result;
+  }
+
+
+  // 3. Check Prime
+  isPrime(n: number) {
+    if (n <= 1) return false;
+    if (n === 2) return true;
+    if (n % 2 === 0) return false;
+
+    for (let i = 3; i <= Math.sqrt(n); i += 2) {
+      if (n % i === 0) return false;
+    }
+    return true;
+  }
+
+  // 4. GCD (Greatest Common Divisor)
+  gcd(a: number, b: number) {
+    while (b !== 0) {
+      [a, b] = [b, a % b];
+    }
+    return Math.abs(a);
+  }
+
+
+
+  // 5. LCM (Least Common Multiple)
+  lcm(a: number, b: number) {
+    return Math.abs(a * b) / this.gcd(a, b);
+  }
+
+
+
+  // 6. Reverse a Number
+  reverseNumber(num: number) {
+    const sign = Math.sign(num);
+    const reversed = parseInt(
+      Math.abs(num).toString().split('').reverse().join('')
+    );
+    return sign * reversed;
+  }
 }
